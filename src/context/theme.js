d4e4db0909dd0f5ac00c8c1245c7c4153eb2c6ab
Boody <cfbuddy124@gmail.com>
2022-03-27 22:03:31 +0200

    add theme context

---
+++ src/context/theme.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  CssBaseline,
+  createTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material";
+
+const ThemeContext = React.createContext();
+ThemeContext.displayName = "themeCtx";
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = React.useState("dark");
+  const currentTheme = createTheme({ palette: { mode: theme } });
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <MuiThemeProvider theme={currentTheme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => React.useContext(ThemeContext);
+
+export { ThemeProvider, useTheme };
