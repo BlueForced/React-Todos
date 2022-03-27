6162362a79f27da1b70c0ca8ee23022af7bdad61
Boody <cfbuddy124@gmail.com>
2022-03-27 22:03:44 +0200

    get app in initial working state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Container from "@mui/material/Container";
+import { ThemeProvider } from "./context/theme";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <ThemeProvider>
+      <Container
+        sx={{
+          display: "flex",
+          position: "absolute",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div>hi</div>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
